53e8c175b93b8d50e84e8e77829802909d86331d
Jake <jacob.m.livingston@gmail.com>
2024-02-12 08:54:21 -0800

    Remove forklift temporarily

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 forkliftLoaded=0
-function loadForklift(){
-    forkliftLoaded+=1
-    console.log(forkliftLoaded);
-}
+// function loadForklift(){
+//     forkliftLoaded+=1
+//     console.log(forkliftLoaded);
+// }
 
-function createForklift(x,y){
-    const forkliftLift0 = new Image();
-    const forkliftLift5 = new Image();
-    const forkliftLift10 = new Image();
-    const forkliftLift15 = new Image();
-    const forkliftLift20 = new Image();
-    const forkliftLift25 = new Image();
-    const forkliftLift30 = new Image();
-    const forkliftLift35 = new Image();
-    const forkliftLift40 = new Image();
-    const forkliftLift45 = new Image();
-    const forkliftLift50 = new Image();
-    const forkliftLiftMove = new Image();
+// function createForklift(x,y){
+//     const forkliftLift0 = new Image();
+//     const forkliftLift5 = new Image();
+//     const forkliftLift10 = new Image();
+//     const forkliftLift15 = new Image();
+//     const forkliftLift20 = new Image();
+//     const forkliftLift25 = new Image();
+//     const forkliftLift30 = new Image();
+//     const forkliftLift35 = new Image();
+//     const forkliftLift40 = new Image();
+//     const forkliftLift45 = new Image();
+//     const forkliftLift50 = new Image();
+//     const forkliftLiftMove = new Image();
 
-    forkliftLift0.src = 'patterns/Forklift/Forklift-lift0.png';
-    forkliftLift5.src = 'patterns/Forklift/Forklift-lift5.png';
-    forkliftLift10.src = 'patterns/Forklift/Forklift-lift10.png';
-    forkliftLift15.src = 'patterns/Forklift/Forklift-lift15.png';
-    forkliftLift20.src = 'patterns/Forklift/Forklift-lift20.png';
-    forkliftLift25.src = 'patterns/Forklift/Forklift-lift25.png';
-    forkliftLift30.src = 'patterns/Forklift/Forklift-lift30.png';
-    forkliftLift35.src = 'patterns/Forklift/Forklift-lift35.png';
-    forkliftLift40.src = 'patterns/Forklift/Forklift-lift40.png';
-    forkliftLift45.src = 'patterns/Forklift/Forklift-lift45.png';
-    forkliftLift50.src = 'patterns/Forklift/Forklift-lift50.png';
-    forkliftLiftMove.src = 'patterns/Forklift/Forklift-move.png';
+//     forkliftLift0.src = 'patterns/Forklift/Forklift-lift0.png';
+//     forkliftLift5.src = 'patterns/Forklift/Forklift-lift5.png';
+//     forkliftLift10.src = 'patterns/Forklift/Forklift-lift10.png';
+//     forkliftLift15.src = 'patterns/Forklift/Forklift-lift15.png';
+//     forkliftLift20.src = 'patterns/Forklift/Forklift-lift20.png';
+//     forkliftLift25.src = 'patterns/Forklift/Forklift-lift25.png';
+//     forkliftLift30.src = 'patterns/Forklift/Forklift-lift30.png';
+//     forkliftLift35.src = 'patterns/Forklift/Forklift-lift35.png';
+//     forkliftLift40.src = 'patterns/Forklift/Forklift-lift40.png';
+//     forkliftLift45.src = 'patterns/Forklift/Forklift-lift45.png';
+//     forkliftLift50.src = 'patterns/Forklift/Forklift-lift50.png';
+//     forkliftLiftMove.src = 'patterns/Forklift/Forklift-move.png';
 
-    // forkliftLift0.addEventListener('load', loadForklift)
-    // forkliftLift5.addEventListener('load', loadForklift) 
-    // forkliftLift10.addEventListener('load', loadForklift) 
-    // forkliftLift15.addEventListener('load', loadForklift)
-    // forkliftLift20.addEventListener('load', loadForklift)
-    // forkliftLift25.addEventListener('load', loadForklift)
-    // forkliftLift30.addEventListener('load', loadForklift)
-    // forkliftLift35.addEventListener('load', loadForklift)
-    // forkliftLift40.addEventListener('load', loadForklift)
-    // forkliftLift45.addEventListener('load', loadForklift)
-    // forkliftLift50.addEventListener('load', loadForklift)
-    // forkliftLiftMove.addEventListener('load', loadForklift)
+//     // forkliftLift0.addEventListener('load', loadForklift)
+//     // forkliftLift5.addEventListener('load', loadForklift) 
+//     // forkliftLift10.addEventListener('load', loadForklift) 
+//     // forkliftLift15.addEventListener('load', loadForklift)
+//     // forkliftLift20.addEventListener('load', loadForklift)
+//     // forkliftLift25.addEventListener('load', loadForklift)
+//     // forkliftLift30.addEventListener('load', loadForklift)
+//     // forkliftLift35.addEventListener('load', loadForklift)
+//     // forkliftLift40.addEventListener('load', loadForklift)
+//     // forkliftLift45.addEventListener('load', loadForklift)
+//     // forkliftLift50.addEventListener('load', loadForklift)
+//     // forkliftLiftMove.addEventListener('load', loadForklift)
 
-    return new Forklift(x,y,75,"#673AB7",{
-        "lift0"   : forkliftLift0,  
-        "lift5"   : forkliftLift5,
-        "lift10"  : forkliftLift10, 
-        "lift15"  : forkliftLift15, 
-        "lift20"  : forkliftLift20,
-        "lift25"  : forkliftLift25,
-        "lift30"  : forkliftLift30,  
-        "lift35"  : forkliftLift35, 
-        "lift40"  : forkliftLift40,
-        "lift45"  : forkliftLift45,
-        "lift50"  : forkliftLift50,
-        "liftMove": forkliftLiftMove
-    })
-}
+//     return new Forklift(x,y,75,"#673AB7",{
+//         "lift0"   : forkliftLift0,  
+//         "lift5"   : forkliftLift5,
+//         "lift10"  : forkliftLift10, 
+//         "lift15"  : forkliftLift15, 
+//         "lift20"  : forkliftLift20,
+//         "lift25"  : forkliftLift25,
+//         "lift30"  : forkliftLift30,  
+//         "lift35"  : forkliftLift35, 
+//         "lift40"  : forkliftLift40,
+//         "lift45"  : forkliftLift45,
+//         "lift50"  : forkliftLift50,
+//         "liftMove": forkliftLiftMove
+//     })
+// }
 grid={}
 window.addEventListener("load", function() {
      const canvas = document.getElementById('myCanvas');
